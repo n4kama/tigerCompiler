6ff4631045e39205059df21a04491519a7bb0030
Etienne Renault <renault@lrde.epita.fr>
2019-02-03 10:56:11 +0000

    2021-tc-base-1.0.

---
+++ src/task/tasks.cc
@@ -0,0 +1,42 @@
+/**
+ ** \file task/tasks.cc
+ ** \brief Task module related tasks.
+ */
+
+#include <iostream>
+
+#include <common.hh>
+#include <task/task-register.hh>
+#define DEFINE_TASKS 1
+#include <task/tasks.hh>
+#undef DEFINE_TASKS
+
+// Task module related tasks' implementation.
+namespace task::tasks
+{
+
+  void
+  tasks_list()
+  {
+    TaskRegister::instance().print_task_list(std::cout);
+  }
+
+  void
+  tasks_graph()
+  {
+    TaskRegister::instance().print_task_graph(std::cout);
+  }
+
+  void
+  tasks_selection()
+  {
+    TaskRegister::instance().print_task_order(std::cout);
+  }
+
+  void
+  time_report()
+  {
+    task_timer.dump_on_destruction(std::cerr);
+  }
+
+} // namespace task::tasks
